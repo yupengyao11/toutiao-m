d6773ed6ffa92e979f9f3652fef8149f54c9e94b
于鹏耀 <pumbaa_yy@163.com>
2022-08-26 16:04:19 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,6 @@
     <h1>黑马头条</h1>
     <div><i class="toutiao toutiao-gengduo"></i></div>
 
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <van-cell title="单元格" is-link />
-    <van-cell title="单元格" is-link value="内容" />
-    <van-cell title="单元格" is-link arrow-direction="down" value="内容" />
   </div>
 </template>
 
