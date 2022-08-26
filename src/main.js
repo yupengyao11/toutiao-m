ac6fb90bb798a75d3b2704e3102d351f428acee1
于鹏耀 <pumbaa_yy@163.com>
2022-08-26 15:53:10 +0800

    8-26引入vant库

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 import './styles/index.less'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
